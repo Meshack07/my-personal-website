7a7e1ee5530040c332fb945fbe61700351fe5105
Meshack07 <hogonna@gmail.com>
2026-09-07 00:08:17 +0100

    Prepare backend for deployment

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ dotenv.config();
 
 const app = express();
 
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 
 // ==============================
@@ -133,8 +133,8 @@ ${message}
 // START SERVER
 // ==============================
 
-app.listen(PORT, () => {
+app.listen(PORT, "0.0.0.0", () => {
 
-    console.log(`Server running at http://localhost:${PORT}`);
+    console.log(`Server running on port ${PORT}`);
 
 });
